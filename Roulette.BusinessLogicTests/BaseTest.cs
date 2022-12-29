f13e99477db5ca8988adf69f1f3237249e06f64b
@nkululeko.sifiso.mahlaba <nkululeko.sifiso.mahlaba@doctorly.de>
2022-12-29 21:20:18 +0200

    Adding unites tests.

---
+++ Roulette.BusinessLogicTests/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+
+namespace Roulette.BusinessLogicTests
+{
+    public abstract class BaseTest
+    {
+        public readonly Faker _faker;
+
+        public const int FAKER_STRING2_LENGTH = 20;
+
+        public BaseTest()
+        {
+            _faker = new Faker("en");
+        }
+    }
+}
--- Roulette.BusinessLogicTests/TransactionBLTests.cs
+++ Roulette.BusinessLogicTests/TransactionBLTests.cs
@@ -1,10 +1,56 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Roulette.BusinessLogic;
+using Roulette.BusinessLogic.DTO.Requests;
+using Roulette.DataAccess.Interfaces;
+using Roulette.Models;
 
 namespace Roulette.BusinessLogicTests
 {
     [TestClass]
-    public class TransactionBLTests
+    public class TransactionBLTests : BaseTest
     {
+        private readonly Mock<IGameTransactionRepository> _gameTransactionRepository = new();
+        private readonly Mock<IPlayerDetailRepository> _playerDetailRepositoryMock = new();
+        private readonly Mock<IUnitOfWork> _unitOfWork = new();
+        private Mock<ILogger<TransactionsBL>> _logger = new();
+        private TransactionsBL _transactionBL;
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            _transactionBL = new TransactionsBL(_unitOfWork.Object, _logger.Object);
+        }
+        
+        [TestMethod]
+        public async Task GetPlayerBalance_Success()
+        {
+            // given
+            var playerDetail = new PlayerDetail()
+            {
+                Id = _faker.Random.Int(),
+                PlayerName = _faker.Random.String2(FAKER_STRING2_LENGTH),
+                Balance = _faker.Random.Int(),
+            };
+
+            bool playerDetailRepositoryGetAsyncHasBeenCalled = false;
+            _playerDetailRepositoryMock.Setup(x => x.GetAsync(It.IsAny<int>()))
+                .Callback(() => playerDetailRepositoryGetAsyncHasBeenCalled = true)
+                .Returns(Task.FromResult(playerDetail));
+
+            var playerBalanceRequest = new PlayerBalanceRequest()
+            {
+                PlayerId = playerDetail.Id                
+            };
+
+            // when
+            var result = await _transactionBL.PlayerBalanceAsync(playerBalanceRequest);
+
+            // then
+            Assert.IsNotNull(result);
+            playerDetailRepositoryGetAsyncHasBeenCalled.Should().BeTrue();
+        }             
     }
 }
